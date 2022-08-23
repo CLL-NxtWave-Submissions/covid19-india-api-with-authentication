8c9bad1dba035cab8d670bc4f0bf6ff0b9a89465
Surya T N <suryatej.nim@yahoo.com>
2022-08-23 07:53:45 +0000

    Implemented server-side handler for client request to login with input user credentials through API end-point: POST /login

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const jwt = require("jsonwebtoken");
 const app = express();
 app.use(express.json());
 
+const secretKeyForJWT = "AUTHORIZATION_SECRET";
+
 const covid19IndiaDatabaseFilePath = path.join(
   __dirname,
   "covid19IndiaPortal.db"
@@ -34,5 +36,47 @@ const initializeDBAndServer = async () => {
 };
 
 initializeDBAndServer();
+
+/*
+    End-Point 1: POST /login
+    ------------
+    To accept or reject user login 
+    request based on input credentials
+    and generate a JSON Web Token (JWT)
+    for authorization to requested resources
+    in subsequent api requests, after successful
+    login.
+*/
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const queryToGetRequestedUserData = `
+    SELECT *
+    FROM user
+    WHERE username = '${username}';
+    `;
+
+  const requestedUserData = await covid19IndiaDBConnectionObj.get(
+    queryToGetRequestedUserData
+  );
+  if (requestedUserData === undefined) {
+    res.status(400);
+    res.send("Invalid user");
+  } else {
+    // Valid user
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      requestedUserData.password
+    );
+    if (isPasswordValid) {
+      const userIdentifiablePayload = { username };
+      const jwtToken = jwt.sign(userIdentifiablePayload, secretKeyForJWT);
+      res.send({ jwtToken });
+    } else {
+      res.status(400);
+      res.send("Invalid password");
+    }
+  }
+});
 
 module.exports = app;
